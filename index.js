1dcb6e5428ef7185eaaeab529571702808a77179
designneexx <webmasterondesign@gmail.com>
2022-09-04 17:16:30 +0300

    main правки

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const express = require('express');
+const express = require('express')
 const { exec } = require('child_process')
 const path = require('path')
 const ip = require('ip')
 
-const app = express();
+const app = express()
 const serverAddress = path.join(__dirname, 'minecraft_server.1.16.1.jar')
 
 app.listen(process.env.PORT || 3007, (port) => {
   console.log(`Example app listening on port ${port}`)
+})
 
+app.get('/', (req, res) => {
   const child = exec(
     `java -Xmx1024M -Xms1024M -jar ${serverAddress} nogui`,
     (error, stdout, stderr) => {
       console.log('load.')
 
-      console.log('stdout: ' + stdout)
-      console.log('stderr: ' + stderr)
-
-      if (error !== null) {
-        console.log('exec error: ' + error)
+      if (error) {
+        return res.send(`hello world, ${ip.address()} ${error}`)
       }
+
+      if (stderr) {
+        return res.send(`hello world, ${ip.address()} ${stderr}`)
+      }
+
+      if (stdout) {
+        return res.send(`hello world, ${ip.address()} ${stdout}`)
+      }
+
+      res.send(`hello world, ${ip.address()} ${stdout}`)
     }
   )
 
   console.log(child)
-});
-
-app.get('/', (req, res) => {
-  res.send(`hello world, ${ip.address()}`);
 })
 
 module.exports = app
